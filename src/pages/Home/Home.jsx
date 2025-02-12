4f58d9af70effbde328f869314c6b1b7bbcc0142
Ahasan Ullah <ahasanullah451@gmail.com>
2025-02-12 13:41:02 +0600

    reviews section added to the homapage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "./Banner/Banner";
 import ContactUs from "./ContactUs/ContactUs";
 import OurStory from "./OurStory/OurStory";
+import Reviews from "./Reviews/Reviews";
 import TopFood from "./TopFood/TopFood";
 
 const Home = () => {
@@ -12,6 +13,8 @@ const Home = () => {
       <TopFood></TopFood>
       {/* Our story section */}
       <OurStory></OurStory>
+      {/* Reviews Section */}
+      <Reviews></Reviews>
       {/* Contact Us Section */}
       <ContactUs></ContactUs>
     </div>
