53328749e357e446525cc1846c3d20fbe044d2ba
Ahasan Ullah <ahasanullah451@gmail.com>
2025-02-12 14:08:52 +0600

    error page modified

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import errorImage from "../../assets/Error.jpg"
 
 const ErrorPage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center space-y-5">
-      <h1 className="text-7xl font-bold">Oops!</h1>
+      {/* <h1 className="text-7xl font-bold">Oops!</h1> */}
+      <div className="flex items-center justify-center">
+        <img className="w-96" src={errorImage} alt="" />
+      </div>
       <p className="text-3xl">404-Page not found</p>
-      <Link to={'/'} className="bg-blue-500 p-2 rounded-lg">Back To Home</Link>
+      <Link to={'/'} className="btn md:btn-lg btn-neutral text-2xl">Back To Home</Link>
     </div>
   );
 };
